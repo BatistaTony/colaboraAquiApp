546fdeca03ae6d2b98f2156e0fdea6cb11a4ef70
BatistaTony <batistatomeoliveira96@gmail.com>
2020-10-30 20:52:59 +0100

    change the illustration for mobile on modal sucess

--- src/components/signUp/buttonBack.tsx
+++ src/components/signUp/buttonBack.tsx
@@ -2,11 +2,12 @@ import { ButtonBackStyle } from "./signUpStyle";
 
 interface IProps {
   classNames?: string;
+  onChange?: () => void;
 }
 
-export default function ButtonBack({ classNames, ...props }: IProps) {
+export default function ButtonBack({ classNames, onChange, ...props }: IProps) {
   return (
-    <ButtonBackStyle className={classNames} {...props}>
+    <ButtonBackStyle className={classNames} onClick={onChange} {...props}>
       <img src="/images/back.png" alt="" /> Voltar ao inicio
     </ButtonBackStyle>
   );
