ed7a3699c7262e55e1d3f3629180d5efcf4051e8
BatistaTony <batistatomeoliveira96@gmail.com>
2020-11-03 22:39:53 +0100

    add hidden overflow to list with just 4 itens

--- src/components/signUp/signUpStyle.ts
+++ src/components/signUp/signUpStyle.ts
@@ -581,9 +581,9 @@ export const FormSelect = styled(FormGroup)`
 export const SelectList = styled.ul`
   list-style: none;
   width: 100%;
-  height: ${({ length }) => (length > 5 ? "380px" : "auto")};
+  height: ${({ length }) => (length > 4 ? "380px" : "auto")};
   position: absolute;
-  overflow-y: ${({ length }) => (length > 5 ? "auto" : "hidden")};
+  overflow-y: ${({ length }) => (length > 4 ? "auto" : "hidden")};
   top: 108px;
   background: white;
   border-radius: 20px;
